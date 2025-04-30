f5065aa45e654989b328083820906cb245663715
Fedor <egorhorishev@gmail.com>
2025-04-30 17:14:53 +0700

    almost ready web pages

--- website/app.py
+++ website/app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/donations')
+def donats():
+    return render_template('donations.html')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
